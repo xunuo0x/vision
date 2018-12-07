4bb3ce845d2a0a73e065b090395f32ceabdf2c00
xunuo0x <1175176908@qq.com>
2018-12-07 21:30:38 +0800

    setData可以回调

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,12 @@
 //index.js
 //获取应用实例
+import * as event from '../../utils/event.js'
 const app = getApp()
 const touchStarting = {
   clientX: 0,
   clientY: 0
 }
-const touchState = {
-  touchEnd: false
-}
+const windowHeight = wx.getSystemInfoSync().windowHeight;
 
 Page({
   data: {
@@ -22,32 +21,78 @@ Page({
     showCenterPlayBtn: false,
     enableProgressGesture: false,
     showProgress: false,
-    playState: false,
+    playState: true,
+    animationShow: true,
+    videos: [
+      {
+        videoUrl: "http://v.kandian.qq.com/shg_753067649_1047_04b4dd8dc4b646e6a88aaa66dad2vide.f20.mp4?dis_k=a613a086491dc11011c995dfed9800e1&dis_t=1544012470",
+        durations: 10,
+        poster: "http://qqpublic.qpic.cn/qq_public_cover/0/0-10000-43EA9FAEE70685E641983C69711ECD58_vsmcut/600"
+      },
+      {
+        videoUrl: "http://v.kd1.qq.com/shg_321_1116_5X00000000000000000000000002Dxan.f822.mp4?dis_k=c4d1d9d7ea4238b0ecb8484fb46111a6&dis_t=1544187088",
+        durations: 10,
+        poster: "http://qqpublic.qpic.cn/qq_public_cover/0/0-10000-5F65D00D480EB888EEF0E4451E8EEED1_vsmcut/600"
+      },
+      {
+        videoUrl: "http://v.kd1.qq.com/shg_321_1116_5X0000000000000000000000000D4Vzo.f822.mp4?dis_k=d327905a47374786078f81fe4e830724&dis_t=1544185708",
+        durations: 10,
+        poster: "http://qqpublic.qpic.cn/qq_public_cover/0/0-10000-40401D133F806549B490F75821C0AEDC_vsmcut/600"
+      }
+    ],
     srcList: [
       "http://v.kandian.qq.com/shg_753067649_1047_04b4dd8dc4b646e6a88aaa66dad2vide.f20.mp4?dis_k=a613a086491dc11011c995dfed9800e1&dis_t=1544012470",
-      "http://v.kandian.qq.com/shg_843086474_1047_23bf79851f9a4c7f9f405a9519f9vide.f20.mp4?dis_k=e1e62116af0b871e17522831bdcec25f&dis_t=1544011923",
-      "http://v.kd1.qq.com/shg_321_1116_5X00000000000000000000000002Dxan.f822.mp4?dis_k=f0b289a306c49555caf2b1934355ce31&dis_t=1544013599"
+      "http://v.kd1.qq.com/shg_321_1116_6X000000000000000000000000r5CNnE.f822.mp4?dis_k=6f0206b7c0c286a102fb43f494bd4331&dis_t=1544152873",
+      "http://v.kd1.qq.com/shg_321_1116_6X000000000000000000000000tjvfuC.f822.mp4?dis_k=92fb699912070692e3420c62f8c49f83&dis_t=1544153942"
     ],
     videoIndex: 0,
     objectFit: "contain"
   },
   bindplay: function() {
-    this.setData({
-      playState: true
-    })
-    touchState.touchEnd = false
   },
   onLoad: function () {
+    this.vvideo = wx.createVideoContext("kdvideo", this)
+    this.videoChange = throttle(this.touchMoveHandler, 200)
+    // 绑定updateVideoIndex事件，更新当前播放视频index
+    event.on('updateVideoIndex', this, function (index) {
+      console.log('event updateVideoIndex:', index)
+      setTimeout(() => {
+        this.animation.translateY(0).step()
+        this.setData({
+          animationShow: true,
+          animation: this.animation.export(),
+          animationImage1: '',
+          animationImage2: '',
+          animationPre: false
+        })
+      }, 800)
+    })
   },
-  videoPlay: function() {
-    let vvideo = wx.createVideoContext("kdvideo", this)
+  onReady: function () {
+    this.animation = wx.createAnimation({
+      duration: 500,
+      transformOrigin: '0 0 0'
+    })
+  },
+  videoPlay: function(index) {
+    this.vvideo.play()
     let playState = !this.data.playState
     if (playState === true) {
       vvideo.play()
     } else {
       vvideo.pause()
     }
-    touchState.touchEnd = false
+    this.setData({
+      playState: playState
+    })
+  },
+  changePlayStatus() {
+    let playState = !this.data.playState
+    if (playState) {
+      this.vvideo.play()
+    } else {
+      this.vvideo.pause()
+    }
     this.setData({
       playState: playState
     })
@@ -59,37 +104,91 @@ Page({
     console.log(touchStarting)
   },
   touchMove(e) {
-    if (touchState.touchEnd) {
+    // this.videoChange(e)
+  },
+  touchEndHandler(e) {
+    if (index === 0 || index === this.data.videos.length - 1) {
       return
     }
-    console.log('------touchMove------')
-    console.log(e)
-    let deltaY = e.touches[0].clientY - touchStarting.clientY
-    console.log(deltaY)
+    let deltaY = e.changedTouches[0].clientY - touchStarting.clientY
+    console.log('deltaY ',deltaY)
+
     let index = this.data.videoIndex
-    if (deltaY > 100) {
-      console.log('-1 切换')
-      index -= 1
+    if (deltaY > 100 && index !== 0) {
+      // 更早地设置 animationShow
       this.setData({
-        videoIndex: index
+        animationPre: true,
+        animationShow: false
+      }, () => {
+        console.log('-1 切换')
+        this.createAnimation(1, index).then((res) => {
+          this.animation.translateY(res.direction * windowHeight).step()
+          this.setData({
+            animation: this.animation.export()
+          })
+          event.emit('updateVideoIndex', res.index)
+        })
       })
-      touchState.touchEnd = true
-    } else if (deltaY < -100) {
+    } else if (deltaY < -100 && index !== (this.data.srcList.length - 1)) {
+      this.setData({
+        animationShow: false
+      })
       console.log('+1 切换')
-      index += 1
-      this.setData({
-        videoIndex: index
+      this.createAnimation(-1, index).then((res) => {
+        console.log(res.direction)
+        this.animation.translateY(res.direction * windowHeight).step()
+        this.setData({
+          animation: this.animation.export()
+        })
+        event.emit('updateVideoIndex', res.index)
       })
-      touchState.touchEnd = true
     }
-    console.log('index ', index)
   },
   touchEnd(e) {
     console.log('------touchEnd------')
-    console.log(e)
+    this.touchEndHandler(e)
   },
   touchCancel(e) {
     console.log('------touchCancel------')
     console.log(e)
+  },
+  createAnimation(direction, index) {
+    // direction为-1，向上滑动，animationImage1为(index)的poster，animationImage2为(index+1)的poster
+    // direction为1，向下滑动，animationImage1为(index-1)的poster，animationImage2为(index)的poster
+    let videos = this.data.videos
+    let animationImage1 = ''
+    let animationImage2 = ''
+    console.log('direction ', direction)
+    console.log('index ', index)
+    if (direction > 0) {
+      // 向下滑
+      animationImage1 = videos[index - 1].poster
+      animationImage2 = videos[index].poster
+      index -= 1
+    } else {
+      // 向上滑
+      animationImage1 = videos[index].poster
+      animationImage2 = videos[index + 1].poster
+      index += 1
+    }
+    this.setData({
+      animationImage1: animationImage1,
+      animationImage2: animationImage2,
+      videoIndex: index
+    })
+    return Promise.resolve({
+      index: index,
+      direction: direction
+    })
   }
 })
+function throttle (fn, delay) {
+  var timer = null;
+  return function () {
+    var context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  }
+}
